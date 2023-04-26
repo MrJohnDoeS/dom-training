023c8fbb80f208e9cb3ac64ded2879c043dc0272
Florent Doe <Mr.Teruin@gmail.com>
2023-04-26 15:14:15 +0200

    Exercice number 1 done

--- js/training.js
+++ js/training.js
@@ -1,10 +1,26 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
 
+let txt = document.getElementById("texte-exo1");  // cibler l element par l id texte-exo1
+console.log(txt);
+let btnToggle = document.getElementById("button-toggle");
+// console.log(btnToggle)
+btnToggle.addEventListener("click", getHideThisText);
+function getHideThisText() {
+    if (txt.style.display === "none") {
+        txt.style.display = "block";
+    } else {
+        txt.style.display = "none";
+    }
+
+}
+
+
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
+window.scrool(left)
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
